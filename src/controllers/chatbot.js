a7cc1e92455566c7af1ee0b74351e092610f3a76
Bogos-Printed <beauclair.nat@gmail.com>
2024-02-23 09:52:23 +0100

    added bot and user message, moved images

--- src/controllers/chatbot.js
+++ src/controllers/chatbot.js
@@ -1,6 +1,6 @@
-import Chatbox from '../views/chatbox';
-import Contacts from '../views/contacts';
 import Nav from '../views/nav';
+import Contacts from '../views/contacts';
+import Chatbox from '../views/chatbox';
 
 const Chatbot = class {
   constructor() {
@@ -14,7 +14,13 @@ const Chatbot = class {
     ${Nav()}
     <div class="d-flex flex-wrap min-vh-100" id="root">
       ${Contacts()}
-      ${Chatbox()}
+      <div class="col-9" id="chatbox">
+        ${Chatbox('metal pipe vfx', 'flabbergasted')}
+        <div class="d-flex position-absolute bottom-0 end-0 mb-1 m-4 " id="bar">
+          <input class="form-control" placeholder="message goes here"></input>
+          <button type="submit" class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i></button>
+        </div>
+      </div>
     </div>
     `;
   }
--- src/views/chatbox.js
+++ src/views/chatbox.js
@@ -1,26 +1,15 @@
-export default () => (
+import botMsg from './botmsg';
+import userMsg from './usermsg';
+
+export default (response, message) => (
   `
-  <div class="col-9" id="chatbox">
-  <div class="ms-1 container-fluid row">
-
-    <div class="bot rounded mt-3 w-50 col-6">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus aut eum nostrum cumque recusandae iste beatae. Vero debitis magni unde, quod autem ipsam perspiciatis?</p>
+  <div class="ps-1 container-fluid row">
+    <div class="ps-1 container-fluid row">
+      ${botMsg(response)}
     </div>
-
-    <div class="col-6"></div>
-  </div>
-  
-  <div class="me-1 container-fluid row">
-    <div class="col-6"></div>
-
-    <div class="user container rounded mt-3 w-50 col-6 ">
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus laborum molestiae quidem sint eos, eaque asperiores inventore velit perferendis, impedit officia? Consequatur animi nisi voluptates!</p>
+    <div class="pe-1 container-fluid row">
+      ${userMsg(message)}
     </div>
-  </div>
-
-  <div class="d-flex position-absolute bottom-0 end-0 mb-1 m-4 " id="bar">
-    <input class="form-control" id="message" placeholder="message goes here"></input>
-    <button type="submit" class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i></button>
   </div>
   `
 );
