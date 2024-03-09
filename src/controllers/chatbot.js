484cf1daec5b2704336178160e4de75261add926
Bogos-Printed <beauclair.nat@gmail.com>
2024-03-09 20:05:38 +0100

    can create one message

--- src/controllers/chatbot.js
+++ src/controllers/chatbot.js
@@ -5,7 +5,8 @@ import Chatbox from '../views/chatbox';
 const Chatbot = class {
   constructor() {
     this.el = document.querySelector('#root');
-    this.data = ['metal pipe vfx', 'flabbergasted', 'chappow', 'something about fish', 'bonk'];
+    this.data = [];
+    this.commands = ['/weather', '/otherAPI'];
     this.dataBot = ['lets groove tonight', 'or not', 'i dont know, im a robot I cant dance', 'like at all'];
     this.bot = ['wa', 'Citizen 17', 'sometime i dream about cheese', 'bonk', 'waaaaa'];
 
@@ -20,16 +21,36 @@ const Chatbot = class {
       <div class="col-9" id="chatbox">
         ${Chatbox(this.data, this.dataBot)}
         <div class="d-flex position-absolute bottom-0 end-0 mb-1 m-4 " id="bar">
-          <input class="form-control" placeholder="message goes here"></input>
-          <button type="submit" class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i></button>
+          <input class="form-control" id="contentMsg" placeholder="message goes here"></input>
+          <button type="submit" id="msgSend" class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i></button>
         </div>
       </div>
     </div>
     `;
   }
 
+  getBar() {
+    const messageButton = document.querySelector('#msgSend');
+    const userInput = document.querySelector('#contentMsg');
+    console.log('ping');
+    console.log(this.commands);
+    messageButton.addEventListener('click', () => {
+      const userMessage = userInput.value;
+      if (userMessage !== ' ') {
+        this.data.push(userMessage);
+
+        userInput.value = ' ';
+      } else if (userMessage === this.commands) {
+        console.log('poing');
+      }
+
+      this.el.innerHTML = this.render();
+    });
+  }
+
   run() {
     this.el.innerHTML = this.render();
+    this.getBar();
   }
 };
 
