dfb517fb724aecd4a9542a31bb8bb95444253011
Bogos-Printed <beauclair.nat@gmail.com>
2024-03-13 23:46:38 +0100

    added API's to code

--- src/controllers/chatbot.js
+++ src/controllers/chatbot.js
@@ -6,7 +6,7 @@ const Chatbot = class {
   constructor() {
     this.el = document.querySelector('#root');
     this.data = [];
-    this.commands = ['/weather', '/otherAPI'];
+    this.commands = ['/bored', '/joke', '/advice', '/otherAPI'];
     this.dataBot = [];
     this.bot = ['The Bot', 'Big Bot', 'Soul weatherman'];
 
@@ -28,6 +28,7 @@ const Chatbot = class {
   updateBox() {
     const chatbox = document.querySelector('#chatbox');
     chatbox.innerHTML = Chatbox(this.data, this.dataBot);
+    this.setupEventListeners();
   }
 
   setupEventListeners() {
@@ -51,12 +52,71 @@ const Chatbot = class {
     if (userMessage === '') return;
 
     this.data.push(userMessage);
-    this.dataBot.push('hello');
     this.updateBox();
     userInput.value = '';
 
     if (this.commands.includes(userMessage)) {
-      this.dataBot.push('waaaaa');
+      if (userMessage === '/joke') {
+        fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist').then((response) => {
+          if (!response.ok) {
+            this.dataBot.push('Sorry to say this, but it seems im out of jokes at the moment');
+          }
+          return response.json();
+        }).then((data) => {
+          const joke = data.joke;
+          const setup = data.setup;
+          const delivery = data.delivery;
+          if (joke != null) {
+            this.dataBot.push(joke);
+            console.log(data.category);
+            this.updateBox();
+          } else {
+            this.dataBot.push(setup + delivery);
+          }
+          this.updateBox();
+        }).catch((error) => {
+          console.error('error with fetch:', error);
+          this.updateBox();
+        });
+      } else if (userMessage === '/bored') {
+        fetch('https://www.boredapi.com/api/activity/').then((response) => {
+          if (!response.ok) {
+            this.dataBot.push('I don t have any activities for you at the moment');
+            this.updateBox();
+          }
+          return response.json();
+        }).then((data) => {
+          const activity = data.activity;
+          if (!activity) {
+            console.log('no activities');
+          }
+          this.dataBot.push(activity);
+          this.updateBox();
+        }).catch((error) => {
+          console.error('error:', error);
+        });
+        this.updateBox();
+      } else if (userMessage === '/advice') {
+        fetch('https://api.adviceslip.com/advice').then((response) => {
+          if (!response.ok) {
+            this.dataBot.push('I don t have any advices for you at the moment');
+            this.updateBox();
+          }
+          return response.json();
+        }).then((data) => {
+          const advice = data.slip.advice;
+          if (!advice) {
+            console.log('no advices');
+          }
+          this.dataBot.push(advice);
+          this.updateBox();
+        }).catch((error) => {
+          console.error('error:', error);
+        });
+        this.updateBox();
+      }
+    } else {
+      this.dataBot.push('hello');
       this.updateBox();
     }
   }
